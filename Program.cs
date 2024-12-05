ec41f4c1052ec90214473af467fe60c6649f0a5c
Ethan Ying Wang <Yingqiu.Wang@mssa.newhorizons.com>
2024-12-05 09:09:00 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Assignment_5._2._3
+{
+    internal class Program
+    {
+        public static void PrintNumbersReverse(int n)
+        {
+            if (n <= 0)
+            {
+                return;
+            }
+
+            Console.Write(n + " ");
+
+            PrintNumbersReverse(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("How many numbers to print: ");
+            int n = int.Parse(Console.ReadLine());
+
+            PrintNumbersReverse(n);
+
+            Console.WriteLine();
+        }
+    }
+}
